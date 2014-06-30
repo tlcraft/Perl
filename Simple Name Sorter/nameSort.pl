bf018cbe96f7e263e144c9d54747abdbd20c033e
tlcraft <tlcraft@users.noreply.github.com>
2014-06-30 14:45:47 -0600

    Add Name Sorter and Organize Files

---
+++ Simple Name Sorter/nameSort.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+# Sorts a list of names from a given file
+# Names are given as 'Last, First' and output as 'First Last'
+use warnings;
+use strict;
+
+my (@fileNames,		# array of input
+    @sortedNames,	# array of sorted names
+    $line,		# an input line (last, first) 
+    $first,		# a first name 
+    $last);		# a last name
+
+chomp (@fileNames = <STDIN>);
+@sortedNames = sort @fileNames;
+
+foreach $line (@sortedNames)
+{
+	($last, $first) = split /, /, $line;
+	print "$first $last\n";
+}
+
+print "\n";
