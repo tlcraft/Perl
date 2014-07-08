c41461cee76af326220dfce0388a38f1da270e92
tlcraft <tlcraft@users.noreply.github.com>
2014-07-08 13:27:27 -0600

    Edit input file. Add new sort example.

---
+++ Simple Name Sorter/nameSort2.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+# Second exmaple of sorting names this time using pattern matching
+# Still takes in a list of names in the 'last, first' format and
+# then prints them in alphabetical order in the 'first last' format
+
+use warnings;
+use strict;
+
+my (	@nameArray,
+	@sortedArray,
+	$line);
+
+chomp( @nameArray = <STDIN> );
+@sortedArray = sort @nameArray;
+
+foreach $line ( @sortedArray ) 
+{
+	$line =~ /^(.*), (.*)$/;
+	print "$2 $1\n";
+}
+
+print "\n";
