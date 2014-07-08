f1bba770b7d90b032f27876898b0d5ce6fadcaad
tlcraft <tlcraft@users.noreply.github.com>
2014-07-08 13:58:27 -0600

    Edit file input

--- Simple Grep/simplePerlGrep.pl
+++ Simple Grep/simplePerlGrep.pl
@@ -3,13 +3,16 @@
 use warnings;
 use strict;
 
-my $line = $ARGV[0]; 		# a line from the input file
+my $file = $ARGV[0]; 		# the input file
 my $searchString = $ARGV[1]; 	# the string to search for
+my $line;			# a line from the input file
 
-print "\nSearching file " . $line . "\n";
+print "\nSearching file " . $file . "\n";
 print "For instances of " . $searchString . "\n\n";
 
-while ($line = <>) 
+open FILE, "<", $file or die $!;
+
+while ($line = <FILE>) 
 {
 	print $line if ($line =~ /$searchString/);
 }
